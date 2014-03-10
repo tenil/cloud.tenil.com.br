3b927b9da66328ecf980da13e49774ce47136606
Roberto Tenil <roberto.tenil@gmail.com>
2014-03-10 17:07:02 -0300

    Conexão com o banco de dados

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -18,8 +18,8 @@ return array(
         'port' => 587, // Notice port change for TLS is 587
         'connection_class' => 'plain',
         'connection_config' => array(
-            'username' => 'AKIAIMQAL354XXTUFRVQ',
-            'password' => 'ApwN9pFWzUkmpsa0LTqODsjz9cSwU+pRE0KIc55uvni3',
+            'username' => 'AKIAIFLK4RVKZEWBW4NA',
+            'password' => 'AgSiyezSIzWH7UvOSj+SM7Zs3ICg1+eAmb8CIYj/3ghr',
             'ssl' => 'tls',
             'from' => 'contato@tenil.com.br',
             'sender' => 'Tenil Tecnologia'
