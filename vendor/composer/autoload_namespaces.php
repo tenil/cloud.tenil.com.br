669461c55a7f4fd8ce0587c942faeb62c55ce180
Roberto Tenil <roberto.tenil@gmail.com>
2015-12-29 12:10:44 -0200

    Atualizações do comporser.phar

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -17,7 +17,6 @@ return array(
     'Doctrine\\Common\\Inflector\\' => array($vendorDir . '/doctrine/inflector/lib'),
     'Doctrine\\Common\\DataFixtures' => array($vendorDir . '/doctrine/data-fixtures/lib'),
     'Doctrine\\Common\\Collections\\' => array($vendorDir . '/doctrine/collections/lib'),
-    'Doctrine\\Common\\Cache\\' => array($vendorDir . '/doctrine/cache/lib'),
     'Doctrine\\Common\\Annotations\\' => array($vendorDir . '/doctrine/annotations/lib'),
     'Doctrine\\Common\\' => array($vendorDir . '/doctrine/common/lib'),
     'DoctrineORMModule\\' => array($vendorDir . '/doctrine/doctrine-orm-module/src'),
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -40,11 +40,20 @@ class ComposerAutoloaderIniteb10a505567a67ecc622f0ee18cb48b4
 
         $loader->register(true);
 
+        $includeFiles = require __DIR__ . '/autoload_files.php';
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireeb10a505567a67ecc622f0ee18cb48b4($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
 
-function composerRequireeb10a505567a67ecc622f0ee18cb48b4($file)
+function composerRequireeb10a505567a67ecc622f0ee18cb48b4($fileIdentifier, $file)
 {
-    require $file;
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+    }
 }
