bd437895237363421c94b69c8b72d66f291de8b2
Roberto Tenil <roberto.tenil@gmail.com>
2016-03-31 18:43:25 -0300

    1. Modificação no autoload

--- public/index.php
+++ public/index.php
@@ -6,8 +6,12 @@
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-    return false;
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
 }
 
 // Setup autoloading
@@ -15,5 +19,3 @@ require 'init_autoloader.php';
 
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
-
-// Arquivo modificado na branch master
