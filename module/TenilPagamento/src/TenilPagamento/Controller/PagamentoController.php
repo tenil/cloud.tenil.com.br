8f9f7a06a8c533635529608a1e424bfed3e5294a
Roberto Tenil <roberto.tenil@gmail.com>
2016-10-26 23:55:39 -0200

    1. composer.phar update

--- module/TenilPagamento/src/TenilPagamento/Controller/PagamentoController.php
+++ module/TenilPagamento/src/TenilPagamento/Controller/PagamentoController.php
@@ -17,15 +17,41 @@ use \PagSeguroAccountCredentials;
 
 use Zend\Config\Config;
 
+use Zend\Math\Rand;
+
+
+use PHPSC\PagSeguro\Credentials;
+use PHPSC\PagSeguro\Environments\Production;
+use PHPSC\PagSeguro\Environments\Sandbox;
+use PHPSC\PagSeguro\Customer\Customer;
+use PHPSC\PagSeguro\Items\Item;
+use PHPSC\PagSeguro\Requests\Checkout\CheckoutService;
+use PHPSC\PagSeguro\Purchases\Transactions\Locator;
+
 class PagamentoController extends AbstractActionController
 {
+
+    private function getCredentials()
+    {
+        $config = $this->getServiceLocator()->get('Config');
+        $credentialsConfig = $config['PagSeguro'];
+
+        $email = $credentialsConfig['credentials']['email'];
+        $token = $credentialsConfig['credentials']['token']['sandbox'];
+
+        return new Credentials(
+            $email,
+            $token,
+            new Sandbox()
+        );
+    }
 
     public function indexAction()
     {
 
         $paymentRequest = new PagSeguroPaymentRequest();
         $paymentRequest->addItem('0001', 'Notebook', 1, 130.00);
-        $paymentRequest->addItem('0002', 'Mochila',  1, 150.99);
+        $paymentRequest->addItem('0002', 'Mochila', 1, 150.99);
 
         $paymentRequest->setSender(
             'João Comprador',
@@ -46,7 +72,7 @@ class PagamentoController extends AbstractActionController
 
         // URL para onde serão enviadas notificações (POST) indicando alterações no status da transação
         $paymentRequest->addParameter('notificationURL', 'http://www.lojamodelo.com.br/nas');
-        
+
         // Você pode definir percentuais de descontos a serem oferecidos com base no meio de pagamento escolhido
         // pelo seu cliente, durante o checkout, no ambiente do PagSeguro.
         $paymentRequest->addPaymentMethodConfig('CREDIT_CARD', 1.00, 'DISCOUNT_PERCENT');
@@ -54,7 +80,7 @@ class PagamentoController extends AbstractActionController
         $paymentRequest->addPaymentMethodConfig('BOLETO', 10.00, 'DISCOUNT_PERCENT');
         $paymentRequest->addPaymentMethodConfig('DEPOSIT', 3.45, 'DISCOUNT_PERCENT');
         $paymentRequest->addPaymentMethodConfig('BALANCE', 0.01, 'DISCOUNT_PERCENT');
-        
+
         $config = $this->getServiceLocator()->get('Config');
 
         $credentialsConfig = $config['PagSeguro'];
@@ -78,6 +104,47 @@ class PagamentoController extends AbstractActionController
         }
 
         return new ViewModel(array('data' => $checkoutUrl));
+    }
+
+    public function testSandboxAction()
+    {
+
+        try {
+            $service = new CheckoutService($this->getCredentials()); // cria instância do serviço de pagamentos
+
+            $checkout = $service->createCheckoutBuilder()
+                ->addItem(new Item(1, $string = Rand::getString(32, 'abcdefghijklmnopqrstuvwxyz', true), $float = Rand::getFloat() * 100))
+                ->addItem(new Item(2, $string2 = Rand::getString(32, 'abcdefghijklmnopqrstuvwxyz', true), $float = Rand::getFloat() * 100))
+                ->getCheckout();
+
+            $response = $service->checkout($checkout);
+
+            //Se você quer usar uma url de retorno
+            $checkout->setRedirectTo($this->url('tenil-pagamento', array('action' => 'retorno')));
+
+            //Se você quer usar uma url de notificação
+            $checkout->setNotificationURL($this->url('tenil-pagamento', array('action' => 'notificacao')));
+
+            header('Location: ' . $response->getRedirectionUrl()); // Redireciona o usuário
+        } catch (Exception $error) { // Caso ocorreu algum erro
+            echo $error->getMessage(); // Exibe na tela a mensagem de erro
+        }
+
+
+    }
+
+    public function transacaoAction()
+    {
+
+        try {
+            $service = new Locator($this->getCredentials()); // Cria instância do serviço de localização de transações
+
+            $transaction = $service->getByCode('2C5C65F1262D47E985566E24D87AC1EA');
+
+            var_dump($transaction); die;// Exibe na tela a transação
+        } catch (Exception $error) { // Caso ocorreu algum erro
+            echo $error->getMessage(); // Exibe na tela a mensagem de erro
+        }
     }
 
 }
